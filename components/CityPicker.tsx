d6e0c309c57229597431338fb87e66aeeec6c3e5
Fabian Gamboa <fabiangamboa95@gmail.com>
2023-05-17 17:18:01 -0400

    city picker created

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,36 +1,82 @@
-"use client";
+'use client'
 
-import { Country, City } from "country-state-city";
-import { useState } from "react";
-import Select from "react-select";
+import {useState} from 'react'
+import Select from 'react-select'
+import {GlobeIcon} from '@heroicons/react/solid'
+import {useRouter} from 'next/navigation'
+import {Country, City} from 'country-state-city'
 
-type CountryOption = {
-  value: {
-    latitude: string;
-    longitude: string;
-    isoCode: string;
-  };
-  label: string;
-};
+type CountryOption = (typeof countries)[0]
 
-type CityOption = {};
+type CityOption = ReturnType<typeof cities>[0]
 
-const options = Country.getAllCountries().map((country) => ({
+const countries = Country.getAllCountries().map(country => ({
   value: {
     latitude: country.latitude,
     longitude: country.longitude,
     isoCode: country.isoCode,
   },
   label: country.name,
-}));
+}))
+
+const cities = (country: CountryOption) =>
+  City.getCitiesOfCountry(country.value.isoCode)?.map(city => ({
+    value: {
+      latitude: city.latitude!,
+      longitude: city.longitude!,
+      countryCode: city.countryCode,
+      name: city.name,
+      stateCode: city.stateCode,
+    },
+    label: city.name,
+  })) ?? []
 
 export default function CityPicker() {
-  const [country, setCountry] = useState<CountryOption>();
-  const [city, setCity] = useState<CityOption>();
+  const [country, setCountry] = useState<CountryOption | null>(null)
+  const [city, setCity] = useState<CityOption | null>(null)
+  const router = useRouter()
+
+  console.log({country, city}) // ! debug
+  if (country) console.log({citiesLength: cities(country).length, citiesSample: cities(country).slice(0, 10)}) // ! debug
 
   return (
-    <div>
-      <Select options={options} value={country} />
+    <div className="space-y-4">
+      <div className="space-y-2">
+        <div className="flex items-center space-x-2 text-white/80">
+          <GlobeIcon className="w-5 h-5 text-white" />
+          <label htmlFor="country">Country</label>
+        </div>
+
+        <Select
+          className="text-black"
+          options={countries}
+          value={country}
+          onChange={option => {
+            setCountry(option)
+            setCity(null)
+          }}
+        />
+      </div>
+
+      {country && (
+        <div className="space-y-2">
+          <div className="flex items-center space-x-2 text-white/80">
+            <GlobeIcon className="w-5 h-5 text-white" />
+            <label htmlFor="country">City</label>
+          </div>
+
+          {/* sta lenta esta madre por ejemplo con US */}
+          <Select
+            className="text-black"
+            options={cities(country)}
+            value={city}
+            onChange={option => {
+              setCity(option)
+              router.push(`location/${option?.value.latitude}/${option?.value.longitude}`)
+            }}
+          />
+        </div>
+      )}
     </div>
-  );
+  )
 }
