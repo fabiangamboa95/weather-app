34216a65ce754597616d6dffcd306196b6f7ba3c
Fabian Gamboa <fabiangamboa95@gmail.com>
2023-05-21 01:53:15 -0400

    ++

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client'
 
-import CityPicker from "@/components/CityPicker";
-import { Card, Divider, Subtitle, Text } from "@tremor/react";
+import CityPicker from '@/components/CityPicker'
+import {Card, Divider, Subtitle, Text} from '@tremor/react'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
-      <Card>
+      <Card className=" max-w-4xl mx-auto">
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
         <Subtitle className="text-xl text-center">Powered by Sonny</Subtitle>
 
@@ -17,5 +17,5 @@ export default function Home() {
         </Card>
       </Card>
     </div>
-  );
+  )
 }
