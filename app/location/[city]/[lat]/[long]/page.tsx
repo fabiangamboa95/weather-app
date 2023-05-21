69221f0f49fda6ffacccd5712ea37762c5b33e5f
Fabian Gamboa <fabiangamboa95@gmail.com>
2023-05-20 20:38:18 -0400

    ready for GPT

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -19,49 +19,51 @@ export default async function WeatherPage({
   const results: Meteo = data.openmeteo
 
   return (
-    <div>
+    <div className=" flex flex-col min-h-screen md:flex-row">
       <InformationPanel props={{city, results, lat, long}} />
 
-      <div className="p-5">
-        <div className="pb-5">
-          <h2 className="text-xl font-bold">Todays Overview</h2>
-          <p className="text-sm text-gray-400">
-            Latest Updated at: {new Date(results.current_weather.time).toLocaleString()} {results.timezone}
-          </p>
-        </div>
-
-        <div className="m-2 mb-10">
-          <CalloutCard message="Chat GPT" />
-        </div>
-
-        <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
-          <StatCard
-            title="Maximum Temperature"
-            metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
-            color="yellow"
-          />
-
-          <StatCard
-            title="Minimum Temperature"
-            metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
-            color="green"
-          />
-
-          <div>
-            <StatCard title="UV Index" metric={results.daily.uv_index_max[0].toFixed(1)} color="rose" />
-            {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
-              <CalloutCard message="UV Index is high today, wear SPF!" warning />
-            )}
+      <div className=" flex-1 p-5 lg:p-10">
+        <div className="p-5">
+          <div className="pb-5">
+            <h2 className="text-xl font-bold">Todays Overview</h2>
+            <p className="text-sm text-gray-400">
+              Latest Updated at: {new Date(results.current_weather.time).toLocaleString()} {results.timezone}
+            </p>
           </div>
 
-          <div className="flex space-x-3">
-            <StatCard title="Wind Speed" metric={`${results.current_weather.windspeed.toFixed(1)}m/s`} color="cyan" />
+          <div className="m-2 mb-10">
+            <CalloutCard message="Chat GPT" />
+          </div>
+
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
+            <StatCard
+              title="Maximum Temperature"
+              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
+              color="yellow"
+            />
 
             <StatCard
-              title="Wind Direction"
-              metric={`${results.current_weather.winddirection.toFixed(1)}°`}
-              color="violet"
+              title="Minimum Temperature"
+              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
+              color="green"
             />
+
+            <div>
+              <StatCard title="UV Index" metric={results.daily.uv_index_max[0].toFixed(1)} color="rose" />
+              {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+                <CalloutCard message="UV Index is high today, wear SPF!" warning />
+              )}
+            </div>
+
+            <div className="flex space-x-3">
+              <StatCard title="Wind Speed" metric={`${results.current_weather.windspeed.toFixed(1)}m/s`} color="cyan" />
+
+              <StatCard
+                title="Wind Direction"
+                metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+                color="violet"
+              />
+            </div>
           </div>
         </div>
       </div>
--- components/InformationPanel.tsx
+++ components/InformationPanel.tsx
@@ -1,6 +1,7 @@
 import weatherCodeToString from '@/lib/weatherCodeToString'
 import CityPicker from './CityPicker'
 import Image from 'next/image'
+import {MoonIcon, SunIcon} from '@heroicons/react/solid'
 
 type Props = {
   props: {
@@ -28,29 +29,20 @@ function InformationPanel({props: {city, results, lat, long}}: Props) {
       <div className=" mt-5 flex items-center justify-between space-x-10 mb-5">
         <div>
           <p>
-            {new Date().toLocaleDateString('en-US', {
-              weekday: 'long',
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-            })}
+            {new Date().toLocaleDateString('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}
           </p>
 
           <p className=" font-extralight">Timezone: {Intl.DateTimeFormat().resolvedOptions().timeZone}</p>
         </div>
 
         <p className=" text-xl font-bold uppercase">
-          {new Date().toLocaleTimeString('en-US', {
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-          })}
+          {new Date().toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true})}
         </p>
       </div>
 
       <hr className="my-10 mb-5" />
 
-      <div>
+      <div className="flex items-center justify-between">
         <div>
           <Image
             src={`https://www.weatherbit.io/static/img/icons/${
@@ -61,9 +53,44 @@ function InformationPanel({props: {city, results, lat, long}}: Props) {
             height={75}
           />
 
-          <div>
-            <p>{results.current_weather.temperature.toFixed(1)}°C</p>
-            <p>{weatherCodeToString[results.current_weather.weathercode].label}</p>
+          <div className=" flex items-center justify-between space-x-10">
+            <p className=" text-6xl font-semibold">{results.current_weather.temperature.toFixed(1)}°C</p>
+
+            <p className=" text-right font-extralight text-lg">
+              {weatherCodeToString[results.current_weather.weathercode].label}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className=" space-y-2 py-5">
+        <div className=" flex items-center space-x-2 px-4 py-3 border border-[#6F90CD] rounded-md bg-[#405885]">
+          <SunIcon className=" h-10 w-10 text-gray-400" />
+
+          <div className=" flex-1 flex justify-between items-center">
+            <p className=" font-extralight">Sunrise</p>
+            <p className=" uppercase text-2xl">
+              {new Date(results.daily.sunrise[0]).toLocaleTimeString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+              })}
+            </p>
+          </div>
+        </div>
+
+        <div className=" flex items-center space-x-2 px-4 py-3 border border-[#6F90CD] rounded-md bg-[#405885]">
+          <MoonIcon className=" h-10 w-10 text-gray-400" />
+
+          <div className=" flex-1 flex justify-between items-center">
+            <p className=" font-extralight">Sunset</p>
+            <p className=" uppercase text-2xl">
+              {new Date(results.daily.sunset[0]).toLocaleTimeString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+              })}
+            </p>
           </div>
         </div>
       </div>
