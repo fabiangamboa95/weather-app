eb34c547ebf2979e96c8b6ce252a559803b3450d
Fabian Gamboa <fabiangamboa95@gmail.com>
2023-05-21 23:36:11 -0400

    ++

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
       <Card className=" max-w-4xl mx-auto">
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
-        <Subtitle className="text-xl text-center">Powered by Sonny</Subtitle>
+        <Subtitle className="text-xl text-center">Powered by Fabbi</Subtitle>
 
         <Divider className="my-10" />
 
